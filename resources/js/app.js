cb9658c14b99788286e54f09f50d5468999f87cf
VITEK_THE_BEST <viktor.luka.00@mail.ru>
2022-03-26 20:40:59 +0500

    add vue 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('home-page', require('./components/HomePage.vue').default);
+
+
+import router from './router';
+
+const app = new Vue({
+    el: '#app',
+    router
+});
---
+++ resources/js/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>hello mather fucker!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
